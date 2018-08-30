3c26293886a983f5bc380af58fb9494fe278397c
Richa Madhupriya <richa.madhupriya@outlook.com>
2018-08-30 16:58:51 +0530

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    ProfileComponent
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
        path: '',
